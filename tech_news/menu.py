0afa607747cf55a97af5529bc674e3a1afac5ff4
debora <debora.r.serra@gmail.com>
2022-11-08 19:35:09 -0300

    feat: add req12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,48 @@
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
+
+
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input(
+        """Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por tag;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair.
+ """
+    )
+    if option == "0":
+        amount = input("Digite quantas notícias serão buscadas:")
+        return get_tech_news(amount)
+    elif option == "1":
+        title = input("Digite o título:")
+        return search_by_title(title)
+    elif option == "2":
+        date = input("Digite a data no formato aaaa-mm-dd:")
+        return search_by_date(date)
+    elif option == "3":
+        tag = input("Digite a tag:")
+        return search_by_tag(tag)
+    elif option == "4":
+        category = input('Digite a categoria:')
+        return search_by_category(category)
+    elif option == '5':
+        return top_5_news()
+    elif option == '6':
+        return top_5_categories()
+    elif option == '7':
+        return
+    else:
+        print('Opção inválida')
+        return
